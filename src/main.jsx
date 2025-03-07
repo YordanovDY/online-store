65fb6ba805b6d1a7c742100539cea038f271907b
YordanovDY <yordanovdy23@gmail.com>
2025-03-07 14:13:53 +0200

    Import public CSS and files

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+
 import App from './App.jsx'
-// TODO: Import CSS
+
+import './styles/core.css';
+import './styles/colors.css';
+import './styles/components.css';
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+    <StrictMode>
+        <App />
+    </StrictMode>,
 )
