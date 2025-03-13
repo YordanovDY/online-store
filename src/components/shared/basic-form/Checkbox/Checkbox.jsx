7e03b350845dc2f0af04b006ca8bbea6575745ba
YordanovDY <yordanovdy23@gmail.com>
2025-03-13 18:16:41 +0200

    Create checkbox component

---
+++ src/components/shared/basic-form/Checkbox/Checkbox.jsx
@@ -0,0 +1,23 @@
+import './Checkbox.css';
+
+export default function Checkbox({name, label}) {
+    return (
+        <div className="checkbox-wrapper-28">
+            <input id="tmp-28" type="checkbox" name={name} className="promoted-input-checkbox" />
+            <svg>
+                <use xlinkHref="#checkmark-28" />
+            </svg>
+            <label htmlFor="tmp-28">{label}</label>
+            <svg xmlns="http://www.w3.org/2000/svg" style={{ display: "none" }}>
+                <symbol id="checkmark-28" viewBox="0 0 24 24">
+                    <path
+                        strokeLinecap="round"
+                        strokeMiterlimit={10}
+                        fill="none"
+                        d="M22.9 3.7l-15.2 16.6-6.6-7.1"
+                    ></path>
+                </symbol>
+            </svg>
+        </div>
+    );
+}
