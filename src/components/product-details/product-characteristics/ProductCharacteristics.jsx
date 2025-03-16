0fa6ba068470b63e436de9cd9104c75bbfa47fa3
YordanovDY <yordanovdy23@gmail.com>
2025-03-16 11:28:05 +0200

    Show boolean characteristics as icons

--- src/components/product-details/product-characteristics/ProductCharacteristics.jsx
+++ src/components/product-details/product-characteristics/ProductCharacteristics.jsx
@@ -1,4 +1,5 @@
 import LoadingSpinner from '../../shared/loading-spinner/LoadingSpinner';
+import PriceItemValue from './price-item-value/PriceItemValue';
 import styles from './ProductCharacteristics.module.css';
 
 export default function ProductCharacteristics({ characteristicsArr = [], pending }) {
@@ -11,12 +12,11 @@ export default function ProductCharacteristics({ characteristicsArr = [], pendin
                     {characteristicsArr.map(item =>
                         <li key={item.char} className={styles['price-item']}>
                             <div className={styles['price-item-key']}>{item.char}</div>
-                            <div className={styles['price-item-value']}>{item.value}</div>
+                            <PriceItemValue value={item.value} />
                         </li>
                     )}
                 </ul>
             }
-
         </div>
     );
 }
---
+++ src/components/product-details/product-characteristics/price-item-value/PriceItemValue.jsx
@@ -0,0 +1,16 @@
+import styles from './PriceItemValue.module.css';
+import './BooleanValue.css';
+
+export default function PriceItemValue({ value }) {
+    let booleanValue = value === 'true' || value === 'false' ? value : null;
+    return (
+        <div className={styles['price-item-value']}>
+            {booleanValue === null
+                ? value
+                : booleanValue === 'true'
+                    ? <i class="fa-solid fa-circle-check boolean-value b-true" />
+                    : <i class="fa-solid fa-circle-xmark boolean-value b-false" />
+            }
+        </div>
+    );
+}
