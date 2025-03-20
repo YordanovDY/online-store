d776a67f24208f4205965a43d54501d42b20aaa3
YordanovDY <yordanovdy23@gmail.com>
2025-03-20 15:22:01 +0200

    Refactor ProductCreate

--- src/components/product/product-create/ProductCreate.jsx
+++ src/components/product/product-create/ProductCreate.jsx
@@ -1,66 +1,20 @@
-import { useNavigate, useParams } from "react-router";
-import { useEffect, useState } from "react";
-import useFetch from "../../../hooks/useFetch";
+import { useParams } from "react-router";
 import ControlledForm from "../../shared/controlled-form/ControlledForm";
 import LoadingSpinner from "../../shared/loading-spinner/LoadingSpinner";
-import { charTemplates, baseProductTemplate } from "../templates";
-import useForm from "../../../hooks/useForm";
-import useMutate from "../../../hooks/useMutate";
+import { useCreateProduct } from "../productApi";
 
 export default function ProductCreate() {
     const { subcategoryId } = useParams();
-
-    const SUBCATEGORY_URL = `/subcategories/${subcategoryId}`;
-    const [pending, subcategory, error] = useFetch(SUBCATEGORY_URL, {});
-    const [subcategoryName, setSubcategoryName] = useState('');
-    const navigate = useNavigate();
-
-    const { mutate } = useMutate('/products/catalog', 'POST');
-
-    const inputs = baseProductTemplate();
-
-    const charInputs = charTemplates[subcategoryId]();
-
-    const { values, changeHandler } = useForm([...inputs, ...charInputs]);
-
-    useEffect(() => {
-        if (error) {
-            console.error(error);
-            // TODO: Implement error handling
-            return;
-        }
-    }, [error]);
-
-    useEffect(() => {
-        const receivedSubcat = Object.entries(subcategory).length > 0;
-
-        if (!receivedSubcat) {
-            return;
-        }
-
-        setSubcategoryName(subcategory.name);
-    }, [subcategory])
-
-
-    const submitHandler = async (e) => {
-        e.preventDefault();
-        const { brand, name, imageUrl, quantity, price, description, ...chars } = values;
-        const characteristicsArr = Object.entries(chars);
-
-        let characteristics = [];
-        for (const [char, value] of characteristicsArr) {
-            characteristics.push({ char, value });
-        }
-
-        const payload = { brand, name: `${brand} ${name}`, imageUrl, quantity: Number(quantity), price: Number(price), description, characteristics, subcategory: subcategoryId };
-
-        try {
-            await mutate(payload);
-            navigate(`/catalog/${subcategoryId}/subcategory?page=1`)
-        } catch (err) {
-            console.error(err.message);
-        }
-    }
+    const {
+        pending,
+        subcategoryName,
+        notificationAlert,
+        inputs,
+        charInputs,
+        values,
+        changeHandler,
+        submitHandler,
+    } = useCreateProduct(subcategoryId);
 
     return (
         <section>
@@ -77,6 +31,7 @@ export default function ProductCreate() {
                     submitHandler={submitHandler}
                 />
             }
+            {notificationAlert}
         </section>
     );
 }
--- src/components/product/productApi.js
+++ src/components/product/productApi.js
@@ -4,6 +4,8 @@ import api from "../../services/api";
 import { baseProductTemplate, charTemplates } from "./templates";
 import { getFormData } from "../../utils/formUtil";
 import useNotification from "../../hooks/useNotification";
+import useFetch from "../../hooks/useFetch";
+import useForm from "../../hooks/useForm";
 
 const baseUrl = '/products/catalog';
 
@@ -12,7 +14,7 @@ export function useUpdateProduct(productId) {
     const [inputs, setInputs] = useState([]);
     const [charInputs, setCharInputs] = useState([]);
     const [productName, setProductName] = useState('');
-    const {notify, notificationAlert} = useNotification();
+    const { notify, notificationAlert } = useNotification();
     const navigate = useNavigate();
 
     useEffect(() => {
@@ -75,6 +77,64 @@ export function useUpdateProduct(productId) {
     }
 }
 
+
+export function useCreateProduct(subcategoryId) {
+    const SUBCATEGORY_URL = `/subcategories/${subcategoryId}`;
+    const [pending, subcategory, error] = useFetch(SUBCATEGORY_URL, {});
+    const [subcategoryName, setSubcategoryName] = useState('');
+    const { notify, notificationAlert } = useNotification();
+    const navigate = useNavigate();
+
+    const inputs = baseProductTemplate();
+
+    const charInputs = charTemplates[subcategoryId]();
+
+    const { values, changeHandler } = useForm([...inputs, ...charInputs]);
+
+    useEffect(() => {
+        if (error) {
+            notify(error.message, 'error');
+            return;
+        }
+    }, [error]);
+
+    useEffect(() => {
+        const receivedSubcat = Object.entries(subcategory).length > 0;
+
+        if (!receivedSubcat) {
+            return;
+        }
+
+        setSubcategoryName(subcategory.name);
+    }, [subcategory])
+
+    const submitHandler = async (e) => {
+        e.preventDefault();
+        const { brand, name, imageUrl, quantity, price, description, ...chars } = values;
+        const characteristics = transformCharacteristics(chars);
+
+        const payload = { brand, name: `${brand} ${name}`, imageUrl, quantity: Number(quantity), price: Number(price), description, characteristics, subcategory: subcategoryId };
+
+        try {
+            await api.post(baseUrl, payload);
+            navigate(`/catalog/${subcategoryId}/subcategory?page=1`)
+
+        } catch (err) {
+            notify(err.message, 'error');
+        }
+    }
+
+    return {
+        pending,
+        subcategoryName,
+        notificationAlert,
+        inputs,
+        charInputs,
+        values,
+        changeHandler,
+        submitHandler,
+    }
+}
 
 function getBaseInputsArr(product) {
     const {
