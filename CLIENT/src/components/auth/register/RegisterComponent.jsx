05c3e9ff799b09df131c0dfaf0759c9c64594b03
YordanovDY <yordanovdy23@gmail.com>
2025-03-22 13:25:12 +0200

    Implement register functionality for customers

--- CLIENT/src/components/auth/register/RegisterComponent.jsx
+++ CLIENT/src/components/auth/register/RegisterComponent.jsx
@@ -1,16 +1,47 @@
-import { Link } from 'react-router';
+import { Link, useNavigate } from 'react-router';
 import ControlledForm from '../../shared/controlled-form/ControlledForm';
 import styles from './RegisterComponent.module.css';
+import useForm from '../../../hooks/useForm';
+import useNotification from '../../../hooks/useNotification';
+import useMutate from '../../../hooks/useMutate';
 
 export default function RegisterComponent() {
+   const { mutate } = useMutate('/auth/register', 'POST');
+   const { notificationAlert, notify } = useNotification();
+   const navigate = useNavigate();
 
    const inputs = [
-      { name: 'email', label: 'Email', type: 'text', placeholder: 'john.doe@gmail.com' },
-      { name: 'password', label: 'Password', type: 'password', placeholder: '***********' },
-      { name: 'repassword', label: 'Repeat Password', type: 'password', placeholder: '***********' }
+      { name: 'email', label: 'Email', type: 'text', placeholder: 'john.doe@gmail.com', value: '' },
+      { name: 'password', label: 'Password', type: 'password', placeholder: '***********', value: '' },
+      { name: 'repassword', label: 'Repeat Password', type: 'password', placeholder: '***********', value: '' }
    ]
 
+   const { values, changeHandler } = useForm(inputs);
+
    const navToLoginPageEl = <p>Already have an account? <Link className="hyperlink" to="/login">Login here.</Link></p>
+
+   const registerHandler = async (e) => {
+      e.preventDefault();
+
+      try {
+         if (values.password !== values.repassword) {
+            values.password = '';
+            values.repassword = '';
+            return notify('Passwords mismatch!', 'error');
+         }
+
+         const result = await mutate({ email: values.email, password: values.password });
+         const email = result.user.email;
+
+         navigate(`/register-confirmation/${email}`);
+
+      } catch (err) {
+         values.password = '';
+         values.repassword = '';
+         notify(err.message, 'error');
+      }
+   }
+
    return (
       <section className={styles['register-section']}>
          <ControlledForm
@@ -18,7 +49,11 @@ export default function RegisterComponent() {
             inputsArray={inputs}
             additionalElement={navToLoginPageEl}
             buttonText="Register"
+            values={values}
+            changeHandler={changeHandler}
+            submitHandler={registerHandler}
          />
+         {notificationAlert}
       </section>
    );
 }
