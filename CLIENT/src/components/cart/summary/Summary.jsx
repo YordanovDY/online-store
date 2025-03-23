25a51e602fdb172e4e3531dba623f1b91cccf758
YordanovDY <yordanovdy23@gmail.com>
2025-03-23 14:11:31 +0200

    Hide Purchase button if the cart is empty

--- CLIENT/src/components/cart/summary/Summary.jsx
+++ CLIENT/src/components/cart/summary/Summary.jsx
@@ -2,6 +2,10 @@ import { Link } from 'react-router';
 import styles from '../CartComponent.module.css';
 
 export default function Summary({ totalPrice }) {
+    const purchaseBtn = totalPrice > 0
+        ? <li><Link to="/orders/place-order" className="button btn-primary">Purchase</Link></li>
+        : ''
+
     return (
         <aside>
             <ul className={styles['items-list']}>
@@ -9,7 +13,7 @@ export default function Summary({ totalPrice }) {
                     <div className={styles['price-item-key-bold']}>Total Price</div>
                     <div className={styles['price-item-value-bold']}>{totalPrice.toFixed(2)}$</div>
                 </li>
-                <li><Link to="/orders/place-order" className="button btn-primary">Purchase</Link></li>
+                {purchaseBtn}
             </ul>
         </aside>
     );
