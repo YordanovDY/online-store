bef22716d198f6f9db9eecab25e6e3cd9091faf5
YordanovDY <yordanovdy23@gmail.com>
2025-03-23 11:57:43 +0200

    Remove labels for checkboxes and radio buttons

--- CLIENT/src/components/shared/basic-form/form-input/FormInput.jsx
+++ CLIENT/src/components/shared/basic-form/form-input/FormInput.jsx
@@ -2,10 +2,16 @@ import styles from './FormInput.module.css';
 import Input from './input/Input';
 
 export default function FormInput({ input }) {
+    let label = <label className={styles['label']} htmlFor={input.name}>{input.label}</label>;
+
+    if (input.type === 'checkbox' || input.type === 'radio') {
+        label = '';
+    }
+
     return (
         <div className={styles['field-border']}>
             <div className={styles['field']}>
-                <label className={styles['label']} htmlFor={input.name}>{input.label}</label>
+                {label}
                 <Input input={input} />
             </div>
         </div>
