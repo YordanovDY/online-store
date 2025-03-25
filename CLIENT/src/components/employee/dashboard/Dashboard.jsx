92056bab7cf830c98cc3cff8c15a6279a2766997
YordanovDY <yordanovdy23@gmail.com>
2025-03-25 13:47:14 +0200

    Show the correct section to users based on their role

--- CLIENT/src/components/employee/dashboard/Dashboard.jsx
+++ CLIENT/src/components/employee/dashboard/Dashboard.jsx
@@ -1,33 +1,44 @@
 import { Link } from 'react-router';
+import { useUserContext } from '../../../contexts/UserContext';
 
 export default function Dashboard() {
-   return (
-      <section className="padding-20 d-flex f-direction-column gap-30">
-         <h2 className="fancy-header">Dashboard</h2>
-         <div className="d-flex f-direction-column gap-20">
-            <article className="d-flex f-direction-column gap-10">
-               <h4 className="header-saparator">STORE MANAGER</h4>
-               <div className="d-flex gap-20">
-                  <Link className="tile-button">Create Product</Link>
-                  <Link className="tile-button">Product Managment</Link>
-                  <Link className="tile-button">Orders Processing</Link>
-               </div>
-            </article>
+    const { user } = useUserContext();
+    return (
+        <section className="padding-20 d-flex f-direction-column gap-30">
+            <h2 className="fancy-header">Dashboard</h2>
+            <div className="d-flex f-direction-column gap-20">
 
-            <article className="d-flex f-direction-column gap-10">
-               <h4 className="header-saparator">SUPPLIER</h4>
-               <div className="d-flex gap-20">
-                  <Link className="tile-button">Delivery Managment</Link>
-               </div>
-            </article>
+                {(user.isStoreManager() || user.isAdmin()) &&
+                    <article className="d-flex f-direction-column gap-10">
+                        <h4 className="header-saparator">STORE MANAGER</h4>
+                        <div className="d-flex gap-20">
+                            <Link className="tile-button">Create Product</Link>
+                            <Link className="tile-button">Product Managment</Link>
+                            <Link className="tile-button">Orders Processing</Link>
+                        </div>
+                    </article>
+                }
 
-            <article className="d-flex f-direction-column gap-10">
-               <h4 className="header-saparator">ADMIN</h4>
-               <div className="d-flex gap-20">
-                  <Link to="/dashboard/create-profile" className="tile-button">Create Profiles</Link>
-               </div>
-            </article>
-         </div>
-      </section>
-   );
+                {(user.isSupplier() || user.isAdmin()) &&
+                    <article className="d-flex f-direction-column gap-10">
+                        <h4 className="header-saparator">SUPPLIER</h4>
+                        <div className="d-flex gap-20">
+                            <Link className="tile-button">Delivery Managment</Link>
+                        </div>
+                    </article>
+                }
+
+
+                {user.isAdmin() &&
+                    <article className="d-flex f-direction-column gap-10">
+                        <h4 className="header-saparator">ADMIN</h4>
+                        <div className="d-flex gap-20">
+                            <Link to="/dashboard/create-profile" className="tile-button">Create Profiles</Link>
+                        </div>
+                    </article>
+                }
+
+            </div>
+        </section>
+    );
 }
