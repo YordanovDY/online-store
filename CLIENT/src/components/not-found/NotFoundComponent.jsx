a841466aec4bf7073298951b14e702ed42a13541
YordanovDY <yordanovdy23@gmail.com>
2025-03-29 10:53:58 +0200

    Fix illegal characters in JSX template

--- CLIENT/src/components/not-found/NotFoundComponent.jsx
+++ CLIENT/src/components/not-found/NotFoundComponent.jsx
@@ -5,7 +5,7 @@ export default function NotFoundComponent() {
         <section className="section-404">
             <h2 className="header-404">404 Page Not Found</h2>
             <i className="fa-solid fa-triangle-exclamation warning-icon"></i>
-            <p>Oops! The page you're looking for doesn't exist. Try checking the URL or go back to the homepage.</p>
+            <p>Oops! The page you are looking for does not exist. Try checking the URL or go back to the homepage.</p>
             <div className="d-flex gap-20">
                 <Link to='/' className="button btn-primary">
                     Home
